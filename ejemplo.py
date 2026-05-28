abf5e2ce5733ce7134ada88887c876904507eff1
juan <jcertuchezapata@gmail.com>
2026-05-28 08:19:23 -0500

    Primer commit: agregando ejemplo.py

---
+++ ejemplo.py
@@ -0,0 +1,21 @@
+num1 = int(input( " ingrese el numero uno "));
+num2 = int(input(" ingrese otro numero "));
+suma = (num1 + num2);
+print ("la suma es: " , suma)
+
+FechaNacimiento = int(input ( " ingrese su fecha de nacimiento "));
+FechaNacimiento = ( 2026 - FechaNacimiento );
+print ( " tu edad es: " , FechaNacimiento);
+
+num1 = int ( input ( " ingresa un numero "));
+if num1 >= 0 : 
+    print ( " su numero es positivo ");
+else:
+    print ( " el numero es negativo ");
+
+num1 = int ( input ( " digite un numero ") );
+num2 = int ( input ( " digite otro numero ") );
+divison = ( num1 / num2 );
+print ( " la division es ; " , divison );
+
+print ("esto es lo que se guarda")
